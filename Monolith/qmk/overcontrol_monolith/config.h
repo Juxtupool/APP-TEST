1fe9b040cf67051713538e1cb5540e4995e60a37
Pulak Nayak <58256815+Juxtupool@users.noreply.github.com>
2026-07-06 19:19:18 +0530

    Delete Monolith/qmk directory

--- Monolith/qmk/overcontrol_monolith/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Reduce debounce latency for faster switch response
-#define DEBOUNCE 5
-
-// Force RP2040 double-tap to enter bootloader
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED_PIN GP25 // RP2040-Zero RGB LED pin
